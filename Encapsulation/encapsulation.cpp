a58db04c001b50beafe10d2d864e6959d2d26efe
shaykotselim <hossainshaykot@gmail.com>
2024-09-02 19:45:57 +0600

    create encapsulations

--- Encapsulation/encapsulation.cpp
+++ Encapsulation/encapsulation.cpp
@@ -1,7 +1,58 @@
 #include<iostream>;
 #include <conio.h>;
 using namespace std;
+
+    class Teacher{
+        // properties
+    private:
+        double salary;
+
+    public:
+        string name;
+        string dept;
+        string subject;
+
+
+        //methods// memberFunctions
+        void changeDept (string newDept){
+            dept = newDept;
+        }
+
+        void setSalary (double newSalary){
+            salary = newSalary;
+        }
+        double getSalary (){
+            return salary;
+        }
+    };
+
 int main(){
-        cout << "This is encapsulation ";
+    Teacher t1;
+    t1.name = "Shaykot";
+    t1.dept = "Textile";
+    t1.changeDept("CSE");
+    t1.setSalary(25000);
+
+    cout << t1.name <<endl;
+    cout << t1.dept <<endl;
+    cout << t1.getSalary();
+
     getch();
 }
+
+/*
+            <-----Definition----->
+
+    class & objects
+
+    Object: Objects are entities in the real world
+    Class: class is like a blue print of these entities.
+
+    Access Modifiers
+
+    Private: data & methods accessible inside class.
+    Public: data & method accessible to everyone.
+    Protected: data & method accessible inside class & to its derived class.
+
+*/
+
